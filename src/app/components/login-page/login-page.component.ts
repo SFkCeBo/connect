99b3c1632896e9c111d11845c6dcaccb406d30da
Marc Fernandez <marc@MacBook-Pro-de-Marc-4.local>
2018-05-11 09:42:35 +0200

    Login OK Log Out OK Registrar OK

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { ToastrModule, ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  public email:string;
+  public password:string;
+
+  constructor(
+    public authService: AuthService,
+    public toastr: ToastrService,
+    public router: Router
+
+  ) { }
 
   ngOnInit() {
+  }
+
+  onSumbitLog(){
+    this.authService.loginEmail(this.email,this.password)
+    .then((res)=>{
+      this.router.navigate(['/']);
+    }).catch((err)=>{
+      this.toastr.error("Fallo Login","No has podido logearte");
+      this.router.navigate(['/login']);
+    });
   }
 
 }
--- src/app/components/navbar-page/navbar-page.component.ts
+++ src/app/components/navbar-page/navbar-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-navbar-page',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar-page.component.css']
 })
 export class NavbarPageComponent implements OnInit {
+  public isLogin: boolean;
+  public nombreUser: string;
+  public emailUser: string;
 
-  constructor() { }
+
+  constructor(
+    public authService: AuthService
+  ) { }
 
   ngOnInit() {
+    this.authService.getAuth().subscribe(auth =>{
+      if(auth){
+        this.isLogin=true;
+        this.nombreUser=auth.displayName;
+        this.emailUser=auth.email;
+      }else{
+        this.isLogin = false;
+      }
+    })
+  }
+
+  onClickLogout(){
+    this.authService.logout();
   }
 
 }
